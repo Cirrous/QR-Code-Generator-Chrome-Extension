bcd2a49f0d9abf9a31b5192dd34b069086bb4847
Cirrous <christianraisch@gmail.com>
2024-07-17 17:19:39 +0200

    added more rules and animation

--- script.js
+++ script.js
@@ -2,5 +2,15 @@ var imgBox = document.getElementById("imgBox");
 var qrImage = document.getElementById("qrImage");
 var qrText = document.getElementById("qrText");
 function generateQR() {
-    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+    if (qrText.value.length > 0) {
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+        imgBox.classList.add("show-img");
+    }
+    else {
+        console.log("No Input was given");
+        qrText.classList.add("error");
+        setTimeout(()=> {
+            qrText.classList.remove("error");
+        }, 1000);
+    }
 }
